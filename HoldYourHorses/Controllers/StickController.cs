de30c23cdce414835a3f70fb78b3ca4b844f2dc8
Yousif Ali <yousifali95@hotmail.co.uk>
2022-07-29 16:48:06 +0200

    Stickcontroller added

---
+++ HoldYourHorses/Controllers/StickController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoldYourHorses.Controllers
+{
+    public class StickController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
