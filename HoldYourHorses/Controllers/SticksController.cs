323d5b0cb53ee32d6a531bb7aac04a3f5b7b00eb
Robin Berg Ekman <robinbergekman92@gmail.com>
2022-08-02 14:52:36 +0200

    Uppdaterat Checkout

--- HoldYourHorses/Controllers/SticksController.cs
+++ HoldYourHorses/Controllers/SticksController.cs
@@ -1,4 +1,5 @@
 ﻿using HoldYourHorses.Models;
+using HoldYourHorses.Views.Sticks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HoldYourHorses.Controllers
@@ -21,6 +22,28 @@ namespace HoldYourHorses.Controllers
         public IActionResult Details(int artikelNr)
         {
             return View(dataService.GetDetailsVM(artikelNr));
+        }
+
+        [HttpGet("checkout")]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost("checkout")]
+        public IActionResult Checkout(CheckoutVM checkoutVM)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            else
+                dataService.Checkout(checkoutVM);
+            return RedirectToAction(nameof(Kvitto));
+        }
+
+        [HttpGet("kvitto")]
+        public IActionResult Kvitto()
+        {
+            return View();
         }
     }
 }
