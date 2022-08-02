10bf925915266a620c16ce612a4c5f4fe711764e
Robin Berg Ekman <robinbergekman92@gmail.com>
2022-08-02 11:27:15 +0200

    La till CheckOut

---
+++ HoldYourHorses/Views/Sticks/CheckoutVM.cs
@@ -0,0 +1,13 @@
+﻿namespace HoldYourHorses.Views.Sticks
+{
+    public class CheckoutVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+    }
+}
