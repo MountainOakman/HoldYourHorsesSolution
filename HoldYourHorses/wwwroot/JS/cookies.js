3616553acc196c8c3686fde068aa94fc1e3eed63
Viktor <Viktor.Holm@awacademy.training>
2022-08-04 10:27:02 +0200

    clean-up _Layout/Added css.file + js.file

---
+++ HoldYourHorses/wwwroot/JS/cookies.js
@@ -0,0 +1,9 @@
+﻿window.onload = function() {
+    document.getElementById("cookiebutton").onclick = function () {
+
+        var overlay = document.getElementById("overlay");
+        var popup = document.getElementById("popup");
+        overlay.style.display = "none";
+        popup.style.display = "none";
+    };
+};
