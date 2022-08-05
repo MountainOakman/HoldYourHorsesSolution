5f9220c7b5ad9688062b13b9a1394d51c98f60f7
Lovisa Johansson <llovisaj@gmail.com>
2022-08-05 13:39:24 +0200

    går nu att trycka enter i sökrutan

--- HoldYourHorses/wwwroot/JS/navbar.js
+++ HoldYourHorses/wwwroot/JS/navbar.js
@@ -3,3 +3,11 @@
     console.log(searchString);
     window.location.href = `/?search=${searchString}`;
 }
+
+var input = document.getElementById("search-input");
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("search-btn").click();
+    }
+});
