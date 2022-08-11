68a59dbf9d99944fc83cb660754f62cc1799822d
Lovisa Johansson <llovisaj@gmail.com>
2022-08-11 11:04:03 +0200

    index

--- HoldYourHorses/wwwroot/JS/index.js
+++ HoldYourHorses/wwwroot/JS/index.js
@@ -268,19 +268,19 @@ function ShowOrHideCompareButton() {
 }
 
 
-function addHeart(svg, artikelNr) {
-    var didAddHeart;
-    await fetch(`/addFavourite/?artikelnr=${artikelnr}`)
-        .then((o) => o.text())
-        .then((o) => (didAdd = o));
-    if ("" +svg.style.fill == "") {
-        svg.style.fill = "rgb(248,48,95)"
-    }
-    else {
-        svg.style.fill = "";
-    }
+//function addHeart(svg, artikelNr) {
+//    var didAddHeart;
+//    await fetch(`/addFavourite/?artikelnr=${artikelnr}`)
+//        .then((o) => o.text())
+//        .then((o) => (didAdd = o));
+//    if ("" +svg.style.fill == "") {
+//        svg.style.fill = "rgb(248,48,95)"
+//    }
+//    else {
+//        svg.style.fill = "";
+//    }
 
-}
+//}
 
 
 ///// Slider JAvascript code /////
