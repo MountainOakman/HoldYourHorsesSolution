400a3253c80940fa6bf6d62c3e822d1c8edbd070
Viktor <Viktor.Holm@awacademy.training>
2022-08-11 11:57:01 +0200

    register css

--- HoldYourHorses/Views/Accounts/RegisterVM.cs
+++ HoldYourHorses/Views/Accounts/RegisterVM.cs
@@ -8,13 +8,14 @@ namespace HoldYourHorses.Views.Accounts
         [Required]
         public string Username { get; set; }
 
+        [Display(Name ="Lösenord")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Repeat password")]
+        [Display(Name = "Repetera lösenord")]
         [Compare(nameof(Password))]
         public string PasswordRepeat { get; set; }
     }
